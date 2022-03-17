4dcc18cc6d3a146566507bdb5e79eb73004ce682
vtadikonda <vtadikonda@vertafore.com>
2022-03-17 19:13:19 +0530

    Modified the change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Multi Container Deployment to Docker
+            Multi Container Deployment to Docker to AWS
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
